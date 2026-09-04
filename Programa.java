eca00c4447eca7a34580da46170ecc2616ec5726
anajuliaaraujo589 <ana.araujo.souza09@escola.pr.gov.br>
2026-09-04 08:15:35 -0300

    adiciona o programa.java

---
+++ Programa.java
@@ -0,0 +1,46 @@
+import model.Conta;
+import model.Despesa;
+import model.Meta;
+import model.Receita;
+
+import java.util.Scanner;
+
+    public class Vintage2000{
+        public static void main(String[] args){
+            Scanner Sc = new Scanner (System.in);
+           
+            String loja ="Vintage2000";
+       
+            Produto.nome = new Produto("Vestido");
+            Produto.tendencia = new Produto("Vestido por cima da calça");
+            Produto.preco = new Produto(80.00);
+           
+           
+    public class MyClass {
+        public static void main(String args[]) {
+            Loja loja=new   Loja();
+           
+            Loja.nome="Vintage2000";
+            Loja.endereco="Avenida Brasil 1223";
+     
+        }  
+    }        
+            class Estilo{
+    String nome;
+    String cor;
+    int quantidade;
+}
+
+public class MyClass {
+    public static void main(String args[]) {
+ Estilo e = new Estilo();
+ 
+   e.nome = "Calca";
+   e.cor = "Preto";
+   e.quantidade = 5;
+ 
+     System.out.println("Estilo: " + e.nome);
+        System.out.println("Cor: " + e.cor);
+        System.out.println("Quantidade: " + e.quantidade);
+    }
+}
