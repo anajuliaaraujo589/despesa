c8a7da346b04ed9da9151c02aaeaa49999bea6f6
anajuliaaraujo589 <ana.araujo.souza09@escola.pr.gov.br>
2026-09-04 08:30:02 -0300

    adiciona o Meta.java

---
+++ model/Meta.java
@@ -0,0 +1,13 @@
+    // Aline lais dos santos pizarro // 
+package model;            
+            public class Meta {
+                
+            // atributos//
+            
+             private String objetivo ;
+             private double valorAlvo ;
+             private double valorPoupado;
+                
+                
+             System.out.printtn ("deseja poupar R$ 200,00 para meta [comprar computador]?"                                                              )   
+            }
